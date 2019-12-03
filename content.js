585c0735405402a02e64fd4e544669071f412b44
Jonny Marshall <jonnymarshall5@gmail.com>
2019-12-03 14:33:33 +1100

    messaging logic done but can't find element by baseURI to reappend to new div

--- content.js
+++ content.js
@@ -1,84 +1,111 @@
 console.log("content.js executed");
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  const playlistSection = document.getElementById("playlist")
-  const videoTitlesHTMLElements = playlistSection.querySelectorAll("#video-title");
-  const titleStringMatcher = /(^[^,]*)\s-\s([^,]*)/
-  const allSongs = []
+  if (request === 'searchForDupes') {
+    const playlistSection = document.getElementById("playlist")
+    const videoTitlesHTMLElements = playlistSection.querySelectorAll("#video-title");
+    const titleStringMatcher = /(^[^,]*)\s-\s([^,]*)/
+    const allSongs = []
 
-  // Create a song object from artist and songTitle
-  class Song {
-    constructor(htmlObject) {
-      this.title = htmlObject.title;
-      this.baseURI = htmlObject.baseURI;
-    }
-
-    // artist () {
-    //   return this.title.match(titleStringMatcher)[1];
-    // }
-
-    // song () {
-    //   return this.title.match(titleStringMatcher)[2];
-    // }
-  }
-
-  // Create an array of Song objects for each song in the playlist
-  videoTitlesHTMLElements.forEach(function(videoTitlesHTMLElement){
-      let song = new Song(videoTitlesHTMLElement);
-      allSongs.push(song);
-  });
-
-  // Sort songs by artist name in alphabetical order
-  allSongs.sort(function(a, b) {
-    var nameA = a.title.toUpperCase(); // ignore upper and lowercase
-    var nameB = b.title.toUpperCase(); // ignore upper and lowercase
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-  });
-
-  // console.log(allSongs);
-
-  // Test for a match of both artist and title
-  function matchTest (songOne, songTwo) {
-    return (songOne.title.toUpperCase() === songTwo.title.toUpperCase()) ? true : false;
-  }
-
-  // Create an array for duplicate songs
-  let duplicates = []
-
-  // Find duplicate songs
-  function dupeFinder(allSongs) {
-    let count = 0;
-    for (let i = 0; i < allSongs.length-1; i++) {
-      // console.log(`the count is ${count}`);
-      // Check to see if artist and song are identical
-      if (matchTest(allSongs[count], allSongs[count+1])) {
-        // Return any songs which are identified as a match
-        console.log(`${allSongs[count].title} - ${allSongs[count+1].title} matched! Here are the objects:`);
-        console.log(allSongs[count]);
-        console.log(allSongs[count+1]);
-        const duplicateOne = document.querySelector(`[baseURI="${allSongs[count].baseURI} - ${allSongs[count+1].baseURI}"]`);
-        const duplicateTwo = document.querySelector(`[baseURI="${allSongs[count+1].baseURI} - ${allSongs[count+1].baseURI}"]`);
-        duplicates.push(allSongs[count]);
-        duplicates.push(allSongs[count]);
-        count++;
+    console.log(`request is ${request}`);
+    console.log(`sender is ${sender}`);
+    console.log(`sendResponse is ${sendResponse}`);
+    // Create a song object from artist and songTitle
+    class Song {
+      constructor(htmlObject) {
+        this.title = htmlObject.title;
+        this.baseURI = htmlObject.baseURI;
       }
-      else {
-        count++;
+
+      // artist () {
+      //   return this.title.match(titleStringMatcher)[1];
+      // }
+
+      // song () {
+      //   return this.title.match(titleStringMatcher)[2];
+      // }
+    }
+
+    // Create an array of Song objects for each song in the playlist
+    videoTitlesHTMLElements.forEach(function(videoTitlesHTMLElement){
+        let song = new Song(videoTitlesHTMLElement);
+        allSongs.push(song);
+    });
+
+    // Sort songs by artist name in alphabetical order
+    allSongs.sort(function(a, b) {
+      var nameA = a.title.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.title.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+    });
+
+    // console.log(allSongs);
+
+    // Test for a match of both artist and title
+    function matchTest (songOne, songTwo) {
+      return (songOne.title.toUpperCase() === songTwo.title.toUpperCase()) ? true : false;
+    }
+
+    // Create an array for duplicate songs
+    let duplicates = []
+
+    // Find duplicate songs
+    function dupeFinder(allSongs) {
+      let count = 0;
+      for (let i = 0; i < allSongs.length-1; i++) {
+        // console.log(`the count is ${count}`);
+        // Check to see if artist and song are identical
+        if (matchTest(allSongs[count], allSongs[count+1])) {
+          // Return any songs which are identified as a match
+          console.log(`${allSongs[count].title} - ${allSongs[count+1].title} matched! Here are the objects:`);
+          console.log(allSongs[count]);
+          console.log(allSongs[count+1]);
+          duplicates.push(allSongs[count]);
+          duplicates.push(allSongs[count]);
+          count++;
+        }
+        else {
+          count++;
+        }
       }
     }
+
+    dupeFinder(allSongs);
+
+    sendResponse(duplicates);
   }
+})
 
-  dupeFinder(allSongs);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.method === 'displayDupes') {
 
-  console.log(duplicates);
+    function findDuplicateHTMLElement (duplicate) {
+      console.log(`entered findDuplicateHTMLElement, here's the object passed:`);
+      debugger
+      return document.querySelector(`[baseURI="${duplicate.baseURI}"]`);
+    }
 
-  sendResponse({count:duplicates.length/2});
-
+    function displayDupes (response) {
+      console.log("entered displayDupes");
+      const pageManager = document.getElementById("page-manager");
+      const resultsDiv = document.createElement('div');
+      pageManager.appendChild(resultsDiv);
+      DuplicateHTMLElements = []
+      request.duplicates.forEach(function(duplicate){
+        DuplicateHTMLElements.push(findDuplicateHTMLElement(duplicate));
+      })
+      DuplicateHTMLElements.forEach(function(duplicate){
+        console.log(duplicate);
+        resultsDiv.appendChild(duplicate);
+      })
+    }
+    displayDupes();
+  }
 })
 
 // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
--- popup.js
+++ popup.js
@@ -5,40 +5,30 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("clicked");
     chrome.tabs.query({currentWindow: true, active: true},
       function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'hi', searchForDupes)
+        chrome.tabs.sendMessage(tabs[0].id, 'searchForDupes', searchForDupes)
     })
   }
 
   function searchForDupes (response) {
     console.log("entered searchForDupes");
+    console.log(response.length/2);
     const div = document.createElement('div');
     const displayDupesButton = document.createElement('button');
     displayDupesButton.setAttribute("id", "displayDupesButton");
-    displayDupesButton.textContent = "Click to view duplicates"
-    div.textContent = `${response.count} duplicate songs found in playlist`;
+    displayDupesButton.textContent = "Click to view duplicates";
+    div.textContent = `${response.length/2} duplicate songs found in playlist`;
     document.body.appendChild(div);
     document.body.appendChild(displayDupesButton);
 
     // Add listener for new button
-    document.querySelector('button').addEventListener('click', triggerDisplayDupes, false)
+    document.getElementById('displayDupesButton').addEventListener('click', triggerDisplayDupes, false)
 
     function triggerDisplayDupes () {
-    console.log("entered triggerDisplayDupes");
-    chrome.tabs.query({currentWindow: true, active: true},
-      function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'hi', displayDupes)
-    })
+      console.log("entered triggerDisplayDupes");
+      chrome.tabs.query({currentWindow: true, active: true},
+        function (tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {method: 'displayDupes', duplicates: response})
+      })
+    }
   }
-  }
-
-  // function displayDupes (response) {
-  //   console.log("entered displayDupes");
-  //   const pageManager = document.getElementById("page-manager");
-  //   const resultsDiv = document.createElement('div');
-  //   const h1 = document.createElement('h1');
-  //   pageManager.appendChild(resultsDiv);
-  //   resultsDiv.appendChild(h1);
-  //   h1.innerText = "HELLO WORLD";
-  // }
-
 }, false)
